f48f1579a239baabd4c8003942e8a1729e70380c
Abhinav Singh <singhabhinavds@gmail.com>
2025-07-18 09:27:43 +0930

    adding a clean .env file to be used

--- src/utils/CryptoUtils.ts
+++ src/utils/CryptoUtils.ts
@@ -2,18 +2,18 @@ let CryptoJSUtil = require("crypto-js");
 
 
 // Get the SALT from the system environment variable
-const SALT = process.env.SALT || "defaultSet";
+const SECRET_KEY = process.env.SECRET_KEY || "defaultSet";
 
 // Encryption function 
 
 export function encrypt(text: string){
-    const cipherText = CryptoJSUtil.AES.encrypt(text, SALT).toString();
+    const cipherText = CryptoJSUtil.AES.encrypt(text, SECRET_KEY).toString();
     return cipherText;
 }
 
 // Decrytption function
 export function decrypt(cipherText: string){
-    const bytes = CryptoJSUtil.AEs.decrypt(cipherText, SALT);
+    const bytes = CryptoJSUtil.AEs.decrypt(cipherText, SECRET_KEY);
     const originalText = bytes.toString(CryptoJSUtil.enc.Utf8);
     return originalText;
 }
